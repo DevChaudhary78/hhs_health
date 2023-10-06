7a6537575d2b410b683b70edcf68eb47d869a4a2
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2023-10-05 23:34:15 -0400

    added the Luhn's algorithm to validate HC number

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -3,7 +3,9 @@
   <input v-model="formData.dateOfBirth" type="text" id="dateOfBirth" required>
 
   <label for="healthCard">Health Card Number: </label>
-  <input v-model="formData.healthCard" type="text" id="healthCard" required>
+  <input @input="luhnAlgorithmHCNChecker" v-model="formData.healthCard" type="text" id="healthCard" required>
+  <p v-if="isValidHealthCard">Valid ✅</p>
+  <p v-else>Invalid ❌</p>
 
   <label for="gender">Gender: </label>
   <select v-model="formData.gender" name="gender" id="gender" required>
@@ -22,7 +24,44 @@ input {
 
 <script>
 export default {
-  props: ['formData']
-}
+  props: ['formData'],
+
+  data() {
+    return {
+      isValidHealthCard: false,
+    }
+  },
+
+  methods: {
+    luhnAlgorithmHCNChecker() {
+      const healthCardNumberStr = this.formData.healthCard.replace(/\s+/g, ''); // removing any spaces using regex
+      const length = healthCardNumberStr.length;
+
+      if (length != 10) {
+        this.isValidHealthCard = false;
+        return;
+      }
+
+      let sum = 0;
+
+      for (let i = length - 2; i >= 0; i -= 2) {
+        let currentNum = parseInt(healthCardNumberStr[i], 10);
+        currentNum *= 2;
+        if (currentNum > 9) {
+          currentNum -= 9;
+        }
+
+        sum += currentNum;
+      }
+
+      for (let i = length - 1; i >= 0; i -= 2) {
+        sum += parseInt(healthCardNumberStr[i], 10);
+      }
+
+      this.isValidHealthCard = sum % 10 === 0;
+    }
+  }
+};
+
 </script>
 
